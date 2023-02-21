823d51006aa7640a9d281ed58a26b659c64cbe93
Christian Dean <christiandeandemesa@gmail.com>
2023-02-21 15:02:15 -0800

    Light and Dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import {Routes, Route} from 'react-router-dom';
+
+import {ThemeProvider, CssBaseline, Box} from '@mui/material';
+
+import Login from './pages/Login';
+
+import {ColorModeContext, useMode} from './theme';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	const [theme, colorMode] = useMode();
+
+	return (
+		// Our created context with the mode, and both memoized functions.
+		<ColorModeContext.Provider value={colorMode}>
+			{/* MUI's context for its theme */}
+			<ThemeProvider theme={theme}>
+				{/* MUI CSS Reset */}
+				<CssBaseline />
+				<Box>
+					<Routes>
+						<Route path='/login' element={<Login />} />
+					</Routes>
+				</Box>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
 }
 
 export default App;
