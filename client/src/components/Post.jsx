6bd75d5a522e9e5e8da86b46fa6a798a2bc48380
Christian Dean <christiandeandemesa@gmail.com>
2023-02-23 20:06:13 -0800

    Post design

---
+++ client/src/components/Post.jsx
@@ -0,0 +1,146 @@
+// POST
+
+import {useState} from 'react';
+
+import {useTheme} from '@mui/material/styles';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Fab from '@mui/material/Fab';
+import FormControl from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Typography from '@mui/material/Typography';
+
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import MessageOutlinedIcon from '@mui/icons-material/MessageOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+
+import {user} from '../data';
+
+function Post({post}) {
+	const theme = useTheme();
+	const {mode} = theme.palette;
+
+	const [open, setOpen] = useState(null);
+
+	// Opens or closes a post's comments menu.
+	const handleCommentsToggle = postId => {
+		if (!open) setOpen(postId);
+		else setOpen(null);
+	};
+
+	return (
+		<Box
+			key={post.id}
+			class={`h-content w-full rounded-md flex flex-col p-4 gap-[20px] font-source ${
+				mode === 'dark' ? 'bg-black' : 'bg-white'
+			}`}
+		>
+			{/* FIRST ROW */}
+			<Box class='flex gap-[20px] items-center'>
+				{/* POST USER AVATAR */}
+				<Avatar alt={post.user} src={post.profilePic || post.user[0]} />
+
+				{/* POST USERNAME */}
+				<Typography class='font-playfair text-lg'>{post.user}</Typography>
+			</Box>
+
+			{/* SECOND ROW */}
+			<Box class='border-t-2 border-b-2 p-2 flex flex-col gap-[10px]'>
+				{/* POST MESSAGE */}
+				{post.message && <Typography>{post.message}</Typography>}
+
+				{/* POST PICTURE */}
+				{post.picture && (
+					<Box
+						component='img'
+						src={post.picture}
+						alt='post image'
+						class='h-full w-full'
+					/>
+				)}
+
+				{/* POST VIDEO */}
+				{post.video && (
+					<Box component='iframe' src={post.video} class='h-content w-content' />
+				)}
+			</Box>
+
+			{/* THIRD ROW */}
+			<Box class='flex gap-[20px]'>
+				{/* LIKES BUTTON */}
+				<Button
+					variant={mode === 'dark' ? 'outlined' : 'contained'}
+					color='error'
+					startIcon={<FavoriteBorderOutlinedIcon />}
+				>
+					{post.likes} Likes
+				</Button>
+
+				{/* COMMENTS BUTTON */}
+				<Button
+					variant={mode === 'dark' ? 'outlined' : 'contained'}
+					color='success'
+					startIcon={<MessageOutlinedIcon />}
+					onClick={() => handleCommentsToggle(post.id)}
+				>
+					{post.comments.length} Comments
+				</Button>
+			</Box>
+
+			{/* COMMENTS MENU */}
+			{open === post.id && (
+				<Box
+					class={`flex flex-col p-4 gap-[20px] font-source ${
+						mode === 'dark' ? 'bg-black' : 'bg-white'
+					}`}
+				>
+					<Box class='flex gap-[10px] items-center'>
+						{/* USER AVATAR */}
+						<Avatar alt={user.username} src={user.profilePic || user.username[0]} />
+
+						{/* COMMENT INPUT */}
+						<Box
+							class={`border-1 w-full pl-4 rounded-full flex items-center ${
+								mode === 'dark' ? 'bg-slate-blue' : 'bg-gray'
+							}`}
+						>
+							<FormControl>
+								<Input disableUnderline={true} />
+							</FormControl>
+						</Box>
+
+						{/* POST BUTTON */}
+						<Fab size='small' color={mode === 'dark' ? 'secondary' : 'info'}>
+							<SendOutlinedIcon />
+						</Fab>
+					</Box>
+
+					{/* COMMENTS CONTAINER */}
+					<List>
+						{post.comments.map(comment => (
+							<ListItem key={comment.id}>
+								{/* COMMENT USER AVATAR */}
+								<ListItemAvatar>
+									<Avatar
+										alt={comment.profilePic}
+										src={comment.profilePic || comment.user[0]}
+									/>
+								</ListItemAvatar>
+
+								{/* COMMENT USERNAME AND COMMENT */}
+								<ListItemText primary={comment.user} secondary={comment.comment} />
+							</ListItem>
+						))}
+					</List>
+				</Box>
+			)}
+		</Box>
+	);
+}
+
+export default Post;
